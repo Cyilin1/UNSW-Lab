886455fad5dd507dacaea747cb531411c0d4ed2c
Cyilin1 <847613236@qq.com>
2023-11-12 21:43:02 +0800

    stage3 finished 99%

--- assignment2/basin.c
+++ assignment2/basin.c
@@ -134,7 +134,7 @@ void stage_2(char *out_filename, char *in_filename)
         exit(1);
     }
 
-    fseek(tabi_file, 4, SEEK_SET);
+    // fseek(tabi_file, 4, SEEK_SET);
     uint8_t num_records = (uint8_t)fgetc(tabi_file);
     fwrite(&num_records, 1, 1, tbbi_file);
 
@@ -206,7 +206,180 @@ void stage_2(char *out_filename, char *in_filename)
 /// @param in_filename A path to where the existing TBBI file is located.
 void stage_3(char *out_filename, char *in_filename)
 {
-    // TODO: implement this.
+    FILE *tbbi_file = fopen(in_filename, "rb");
+    FILE *tcbi_file = fopen(out_filename, "wb");
+
+    if (!tbbi_file || !tcbi_file)
+    {
+        perror("Error opening files");
+        exit(1);
+    }
+
+    // Write TCBI magic number.
+    char *magic_value = "TCBI";
+    fwrite(magic_value, 1, 4, tcbi_file);
+
+    // Read and check the magic number.
+    char magic[4];
+    if (fread(magic, 1, 4, tbbi_file) != 4 || strncmp(magic, "TBBI", 4) != 0)
+    {
+        perror("Error: Invalid magic number in TBBI file");
+        exit(1);
+    }
+
+    // Count the number of records in the TBBI file.
+    // Read and count TBBI records here.
+    uint8_t num_records = (uint8_t)fgetc(tbbi_file);
+    fwrite(&num_records, 1, 1, tcbi_file);
+    // Process each record in the TBBI file.
+    for (uint8_t record = 0; record < num_records; record++)
+    {
+        // Read and process TBBI record.
+        uint16_t pathname_length = ReadLittleEndian(tbbi_file, 2);
+        printf("%u\n", pathname_length);
+        WriteLittleEndian(tcbi_file, pathname_length, 2);
+        char pathname[pathname_length + 1];
+        if (fread(pathname, sizeof(char), pathname_length, tbbi_file) != pathname_length)
+        {
+            perror("Error reading pathname from TABI");
+            exit(1);
+        }
+        printf("%s\n", pathname);
+
+        pathname[pathname_length] = '\0';
+        fwrite(pathname, 1, pathname_length, tcbi_file);
+        FILE *receive_file = fopen(pathname, "rb");
+        if (receive_file == NULL)
+        {
+            continue;
+        }
+        uint32_t num_blocks = ReadLittleEndian(tbbi_file, 3);
+
+        struct stat file_stat;
+        if (stat(&pathname, &file_stat) != 0)
+        {
+            perror("无法获取文件大小");
+            exit(1);
+        }
+        // write isDir
+        char mode_isDIR = S_ISDIR(file_stat.st_mode) ? 'd' : '-';
+        fwrite(&mode_isDIR, 1, 1, tcbi_file);
+        // write Owner
+        char mode_usr_pms[3];
+        mode_usr_pms[0] = (file_stat.st_mode & S_IRUSR) ? 'r' : '-';
+        mode_usr_pms[1] = (file_stat.st_mode & S_IWUSR) ? 'w' : '-';
+        mode_usr_pms[2] = (file_stat.st_mode & S_IXUSR) ? 'x' : '-';
+        fwrite(mode_usr_pms, 1, 3, tcbi_file);
+        // write Group
+        char mode_grp_pms[3];
+        mode_grp_pms[0] = (file_stat.st_mode & S_IRGRP) ? 'r' : '-';
+        mode_grp_pms[1] = (file_stat.st_mode & S_IWGRP) ? 'w' : '-';
+        mode_grp_pms[2] = (file_stat.st_mode & S_IXGRP) ? 'x' : '-';
+        fwrite(mode_grp_pms, 1, 3, tcbi_file);
+        // write Others
+        char mode_oth_pms[3];
+        mode_oth_pms[0] = (file_stat.st_mode & S_IROTH) ? 'r' : '-';
+        mode_oth_pms[1] = (file_stat.st_mode & S_IWOTH) ? 'w' : '-';
+        mode_oth_pms[2] = (file_stat.st_mode & S_IXOTH) ? 'x' : '-';
+        fwrite(mode_oth_pms, 1, 3, tcbi_file);
+        // size
+        uint32_t file_size = file_stat.st_size;
+        WriteLittleEndian(tcbi_file, file_size, 4);
+        //
+        uint32_t num_need_update = 0;
+        uint16_t index_capacity = 8;
+        uint32_t *array = (uint32_t *)malloc(index_capacity * sizeof(uint32_t));
+        if (array == NULL)
+        {
+            perror("Memory allocation error");
+            exit(1);
+        }
+        uint32_t num_match_bytes = num_tbbi_match_bytes(num_blocks);
+
+        for (int i = 0; i < num_match_bytes; i++)
+        {
+            uint8_t match_byte;
+            if (fread(&match_byte, 1, 1, tbbi_file) != 1)
+            {
+                perror("x");
+                exit(1);
+            }
+
+            // 统计字节中二进制位为0的个数
+            for (int k = 0; k < 8; k++)
+            {
+                int index = i * 8 + k;
+                if ((match_byte & (1 << (7 - k))) == 0)
+                {
+                    // 输出位置信息
+                    // printf("i=%d, k=%d\n", i, k);
+                    if (index >= num_blocks)
+                        break;
+
+                    array[num_need_update] = index;
+                    num_need_update++;
+                    if (num_need_update >= index_capacity)
+                    {
+                        // 如果动态数组已满，需要重新分配更大的内存
+                        index_capacity *= 2;
+                        uint32_t *new_array = (uint32_t *)realloc(array, index_capacity * sizeof(uint32_t));
+
+                        if (new_array == NULL)
+                        {
+                            perror("Memory reallocation error");
+                            free(array); // 释放旧内存
+                            exit(1);
+                        }
+                        array = new_array;
+                    }
+                    // printf("%d\n", index);
+                }
+                else if (index + 1 > num_blocks)
+                {
+                    perror("x");
+                    exit(1);
+                }
+            }
+        }
+        WriteLittleEndian(tcbi_file, num_need_update, 3);
+        // printf("%u\n", num_need_update);
+        for (int i = 0; i < num_need_update; i++)
+        {
+            WriteLittleEndian(tcbi_file, array[i], 3);
+            uint32_t update_data_len;
+            // printf("%d", 62 / 256);
+            // todo
+            // 如果是最后一块可以直接算
+            if (array[i] != (num_blocks - 1))
+            {
+                update_data_len = 256;
+            }
+            else
+            {
+                update_data_len = file_size - 256 * (num_blocks - 1);
+            }
+            // printf("%u\n", update_data_len);
+            WriteLittleEndian(tcbi_file, update_data_len, 2);
+
+            char update_data[update_data_len];
+            fseek(receive_file, array[i] * 256, SEEK_SET);
+            fread(update_data, 1, update_data_len, receive_file);
+            fwrite(update_data, 1, update_data_len, tcbi_file);
+        }
+
+        free(array);
+
+        // // printf("%u", num_need_update);
+    }
+    // Close the files.
+    char temp;
+    if (fread(&temp, 1, 1, tbbi_file) != 0)
+    {
+        fprintf(stderr, "Error: Extra data found in the TABI file");
+        exit(1);
+    }
+    fclose(tbbi_file);
+    fclose(tcbi_file);
 }
 
 /// @brief Apply a TCBI file to the filesystem.
